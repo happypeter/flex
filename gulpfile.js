653f1580edaa3d5d8e8862318740041b14555256
Billie Zhang <billiecoder@gmail.com>
2015-12-15 11:54:46 +0800

    fixed footer and header

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
 
-gulp.task('build', function () {
+gulp.task('cp', function () {
     return gulp.src('src/layout/index.html')
         .pipe(gulp.dest('dist'));
 });
@@ -16,29 +15,15 @@ function handleError(err) {
 gulp.task('sass', function () {
     return gulp.src('src/style/main.scss')
         .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
+            includePaths: ['scss']
         })).on('error', handleError)
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('dist/style'))
-        .pipe(browserSync.reload({stream:true}));
-});
-
-gulp.task('rebuild', ['build'], function () {
-    browserSync.reload();
-});
-
-gulp.task('browser-sync', ['sass', 'build'], function() {
-    browserSync({
-        server: {
-            baseDir: 'dist'
-        }
-    });
+        .pipe(gulp.dest('dist/style'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['src/layout/index.html'], ['rebuild']);
+    gulp.watch(['src/layout/index.html'], ['cp']);
     gulp.watch(['src/style/*'], ['sass']);
 });
 
-gulp.task('default', ['browser-sync','watch']);
+gulp.task('default', ['cp', 'sass', 'watch']);
