6d2787238902cc60ebb5924f782c45c7310eb8b9
Peter Wang <happypeter1983@gmail.com>
2015-12-17 17:04:42 +0800

    gulp-wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
+
+gulp.task('build', function(){
+  gulp.src("pages/*.html")
+      .pipe(wrap({src:"layout/default.html"}))
+      .pipe(gulp.dest(".."));
+});
 
 function handleError(err) {
   console.log(err.toString());
@@ -15,14 +22,9 @@ gulp.task('sass', function(){
       .pipe(gulp.dest('../styles'));
 });
 
-gulp.task('cp', function(){
-  gulp.src('index.html')
-      .pipe(gulp.dest('..'));
-});
-
 gulp.task('watch', function(){
-  gulp.watch(['*.html'], ['cp']);
+  gulp.watch(['**/*.html'], ['build']);
   gulp.watch(['styles/*.scss'], ['sass']);
 })
 
-gulp.task('default', ['sass', 'cp', 'watch']);
+gulp.task('default', ['sass', 'build', 'watch']);
