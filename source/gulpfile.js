20f4f7d8443e3c37cd150d8979d1eebcd8eac520
Peter Wang <happypeter1983@gmail.com>
2015-12-18 15:05:25 +0800

    browsersync

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
+
+
+gulp.task('browser-sync', ['build', 'sass'], function() {
+    browserSync({
+        server: {
+            baseDir: '..'
+        }
+    });
+});
 
 gulp.task('build', function(){
   gulp.src("pages/*.html")
@@ -19,12 +29,17 @@ gulp.task('sass', function(){
   gulp.src('styles/main.scss')
       .pipe(sass()).on('error', handleError)
       .pipe(prefix())
-      .pipe(gulp.dest('../styles'));
+      .pipe(gulp.dest('../styles'))
+      .pipe(browserSync.reload({stream:true}));;
+});
+
+gulp.task('rebuild', ['build'], function () {
+    browserSync.reload();
 });
 
 gulp.task('watch', function(){
-  gulp.watch(['**/*.html'], ['build']);
+  gulp.watch(['**/*.html'], ['rebuild']);
   gulp.watch(['styles/*.scss'], ['sass']);
 })
 
-gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
